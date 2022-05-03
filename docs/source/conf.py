097f99131262578a713f0c94bf16734f5595ab5f
workwallis <51852621+workwallis@users.noreply.github.com>
2022-05-03 17:21:53 +0200

    Update conf.py

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -2,9 +2,9 @@
 
 # -- Project information
 
-project = 'Lumache'
+project = 'WorkWallis'
 copyright = '2021, Graziella'
-author = 'Graziella'
+author = 'F. Jordan'
 
 release = '0.1'
 version = '0.1.0'
